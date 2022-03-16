b9d77e5c84502efd54a2e14f370d34ea865e4cf3
appletime81 <itrimsl8696@gmail.com>
2022-03-16 22:37:27 +0800

    Update

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import re
 import argparse
+import html
 from tokenize import String
-from typing import Dict
+from typing import Dict, List
 from lxml import etree
 
 def hdl_argument(argument: String):
@@ -15,36 +16,28 @@ def hdl_argument(argument: String):
             argument_dict[argument_split_list[i - 1]] = item
     return argument_dict
 
+def replace_garbled(content: List):
+    pass
+
 
 def change_content(fileName, **kwargs: Dict):
     new_lines = []
-    with open(f'{fileName}', 'r') as f:
+    with open(f'{fileName}', 'r', encoding='utf-8') as f:
         lines = f.readlines()
 
-    # for key, value in kwargs.items():
-    #     for i, line in enumerate(lines):
-    #         if re.search(f'<{key}>', lines[i]):
-    #             print(list(lines[i + 1]))
-    #             temp_text = ''.join([item for item in line if item != ' '])
-    #             lines[i] = lines[i].replace(f'{temp_text}', f'<{key}>{value}</{key}>')
-    #             print(lines[i])
-    #         new_lines.append(lines[i])
-
+    for key, value in kwargs.items():
+        for i, line in enumerate(lines):
+            if re.search(f'<{key}>', lines[i]):
+                temp_text = ''.join([item for item in line if item != ' '])
+                lines[i] = lines[i].replace(f'{temp_text}', f'<{key}>{value}</{key}>')
+            new_lines.append(lines[i])
 
     str_xml = ''
     for i, line in enumerate(lines):
-        if 'numDlSlot>3<' in line:
-            pass
-            # print(list(line))
-            # print(list(lines[i + 1]))
         str_xml += line
-    # with open(f'{fileName}', 'w') as f:
-    #     f.writelines(lines)
-
     et = etree.fromstring(str_xml.encode('utf-8'))
-    # et = et.getroot()
     et = et.getroottree()
-    et.write('output.xml', pretty_print=True, encoding='utf-8')
+    et.write('output.xml', pretty_print=True)
 
 
 if __name__ == '__main__':
@@ -56,3 +49,5 @@ if __name__ == '__main__':
     file_name = args.file
     argument_dict = hdl_argument(args.tag)
     change_content(fileName=file_name, **argument_dict)
+
+    # python main.py --tag "numDlSlot 20" --file output.xml
--- test_lxml_change_content.py
+++ test_lxml_change_content.py
@@ -1,6 +1,5 @@
 import os
-from pprint import pprint
-from re import L
+
 try:
     from lxml import etree
 except ModuleNotFoundError:
@@ -8,7 +7,6 @@ except ModuleNotFoundError:
 
 
 def load_content(fileName):
-
     tag_prefix = '{urn:rdns:com:radisys:nr:gnb}'
     tree = etree.parse(f'{fileName}')
     root = tree.getroot()
@@ -16,39 +14,38 @@ def load_content(fileName):
     # 找出需被修改的節點
     find = etree.XPath("/*/*[13]/*[17]/*[7]/*[6]")
     cdata_text = find(tree)[0].text.strip()
-    print(cdata_text)
     cdata_root = etree.fromstring(cdata_text)
-    cdata_tree = etree.ElementTree(cdata_root)
+
+    # 更改節點內容
+    cdata_root = change_data(root=cdata_root, prefix=tag_prefix)
+
+    # 儲存檔案
+    save_data(tree, root, cdata_root, find)
 
 
-    elements = cdata_root.xpath("//*")
+def change_data(root, prefix):
+    elements = root.xpath("//*")
     for item in elements:
-        if 'numDlSlot' == item.tag.replace(tag_prefix, ''):
+        if 'numDlSlot' == item.tag.replace(prefix, ''):
             item.text = str(20)
-    # print(etree.tostring(cdata_root))
-    find(tree)[0].text = etree.tostring(cdata_root)
+    return root
+
+
+def save_data(tree, root, target_root, find):
+    find(tree)[0].text = etree.tostring(target_root)
     et = etree.ElementTree(root)
-    et.write('output.xml', pretty_print=True, encoding='unidoe')
+    et.write('output.xml', pretty_print=True)
 
-    # with open('output.xml', 'r') as f:
-    #     lines = f.readlines()
-    
-    # new_lines = []
-    # for i, line in enumerate(lines):
-    #     line = line.replace('&lt;', '<').replace('&gt;', '>')
-    #     new_lines.append(line)
-    
-    # with open('output.xml', 'w') as f:
-    #     f.writelines(new_lines)
+    with open('output.xml', 'r') as f:
+        lines = f.readlines()
 
+    new_lines = []
+    for i, line in enumerate(lines):
+        line = line.replace('&lt;', '<').replace('&gt;', '>')
+        new_lines.append(line)
 
-
-def change_data():
-    pass
-
-
-def save_data():
-    pass
+    with open('output.xml', 'w') as f:
+        f.writelines(new_lines)
 
 
 if __name__ == '__main__':
