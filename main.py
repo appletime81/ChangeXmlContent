98c5fbfa46a297475c594c4da2780c4e114a3f2e
Frank <itrimsl8696@gmail.com>
2022-03-30 19:34:36 +0800

    Update

--- main.py
+++ main.py
@@ -4,45 +4,151 @@ import xml.etree.ElementTree as ET
 
 # 全局變數
 unique_id = 1
-input_file_name = "/home/pegauser/synergy/config/oam_sysrepo_du.xml"
-output_file_name = "/home/pegauser/synergy/config/oam_sysrepo_du.xml"
+input_file_name = "oam_template_du_wnc.xml"
+output_file_name = "oam_template_du_wnc.xml"
 root = ET.parse(input_file_name).getroot()
 
-# get environment variables
-NUMDLSLOT = os.environ.get("NUMDLSLOT")
-NUMDLSYMBOL = os.environ.get("NUMDLSYMBOL")
-NUMULSLOT = os.environ.get("NUMULSLOT")
-NUMULSYMBOL = os.environ.get("NUMULSYMBOL")
+# NUMSLOTPARAMS = os.environ.get("NUMSLOTPARAMS")
+NUMSLOTPARAMS = "1"
+
+case_name_dict = {
+    "1": "DDDSU",
+    "2": "DDDSU(WG8)",
+    "3": "DDDDDDDSUU",
+    "4": "DDDSUDDSUU",
+    "5": "DSUUU",
+    "6": "DDSUUUUUUU",
+    "7": "DDDSUUDDSU",
+    "8": "DDDSUUDDDD",
+}
+
+case_params_dict = {
+    "1": {
+        "numDlSlot": "3",
+        "numDlSymbol": "12",
+        "numUlSlot": "1",
+        "numUlSymbol": "0",
+        "p2Pres": "0",
+        "numDlSlotP2": "3",
+        "numDlSymbolP2": "12",
+        "numUlSlotP2": "1",
+        "numUlSymbolP2": "0",
+        "prachCfgIdx": "145",
+    },
+    "2": {
+        "numDlSlot": "3",
+        "numDlSymbol": "10",
+        "numUlSlot": "1",
+        "numUlSymbol": "2",
+        "p2Pres": "0",
+        "numDlSlotP2": "3",
+        "numDlSymbolP2": "12",
+        "numUlSlotP2": "1",
+        "numUlSymbolP2": "0",
+        "prachCfgIdx": "145",
+    },
+    "3": {
+        "numDlSlot": "7",
+        "numDlSymbol": "10",
+        "numUlSlot": "2",
+        "numUlSymbol": "2",
+        "p2Pres": "0",
+        "numDlSlotP2": "7",
+        "numDlSymbolP2": "10",
+        "numUlSlotP2": "2",
+        "numUlSymbolP2": "2",
+        "prachCfgIdx": "145",
+    },
+    "4": {
+        "numDlSlot": "3",
+        "numDlSymbol": "12",
+        "numUlSlot": "1",
+        "numUlSymbol": "0",
+        "p2Pres": "1",
+        "numDlSlotP2": "2",
+        "numDlSymbolP2": "12",
+        "numUlSlotP2": "2",
+        "numUlSymbolP2": "0",
+        "prachCfgIdx": "145",
+    },
+    "5": {
+        "numDlSlot": "1",
+        "numDlSymbol": "12",
+        "numUlSlot": "3",
+        "numUlSymbol": "0",
+        "p2Pres": "0",
+        "numDlSlotP2": "1",
+        "numDlSymbolP2": "12",
+        "numUlSlotP2": "3",
+        "numUlSymbolP2": "0",
+        "prachCfgIdx": "145",
+    },
+    "6": {
+        "numDlSlot": "2",
+        "numDlSymbol": "12",
+        "numUlSlot": "7",
+        "numUlSymbol": "0",
+        "p2Pres": "0",
+        "numDlSlotP2": "2",
+        "numDlSymbolP2": "12",
+        "numUlSlotP2": "7",
+        "numUlSymbolP2": "0",
+        "prachCfgIdx": "145",
+    },
+    "7": {
+        "numDlSlot": "3",
+        "numDlSymbol": "12",
+        "numUlSlot": "2",
+        "numUlSymbol": "0",
+        "p2Pres": "1",
+        "numDlSlotP2": "2",
+        "numDlSymbolP2": "12",
+        "numUlSlotP2": "1",
+        "numUlSymbolP2": "0",
+        "prachCfgIdx": "145",
+    },
+    "8": {
+        "numDlSlot": "3",
+        "numDlSymbol": "12",
+        "numUlSlot": "2",
+        "numUlSymbol": "0",
+        "p2Pres": "1",
+        "numDlSlotP2": "4",
+        "numDlSymbolP2": "12",
+        "numUlSlotP2": "0",
+        "numUlSymbolP2": "0",
+        "prachCfgIdx": "156",
+    },
+}
 
 
 # 遍歷所有的節點
 def walkData(root_node, level, result_list):
     global unique_id
 
-    if unique_id == 111 and level == 5 and "vsData" in root_node.tag and NUMDLSLOT and NUMDLSYMBOL and NUMULSLOT and NUMULSYMBOL:
-        numDlSlotPart = root_node.text[re.search(f"<numDlSlot>\d+</numDlSlot>", root_node.text).start():re.search(
-            f"<numDlSlot>\d+</numDlSlot>", root_node.text).end()]
-        numDlSymbolPart = root_node.text[re.search(f"<numDlSymbol>\d+</numDlSymbol>", root_node.text).start():re.search(
-            f"<numDlSymbol>\d+</numDlSymbol>", root_node.text).end()]
-        numUlSlotPart = root_node.text[re.search(f"<numUlSlot>\d+</numUlSlot>", root_node.text).start():re.search(
-            f"<numUlSlot>\d+</numUlSlot>", root_node.text).end()]
-        numUlSymbolPart = root_node.text[re.search(f"<numUlSymbol>\d+</numUlSymbol>", root_node.text).start():re.search(
-            f"<numUlSymbol>\d+</numUlSymbol>", root_node.text).end()]
+    if unique_id == 121 and level == 5 and "vsData" in root_node.tag:
+        params_dict = case_params_dict.get(NUMSLOTPARAMS)
+        print(f"Your slot case is {case_name_dict.get(NUMSLOTPARAMS)}")
+        for param_name, value in params_dict.items():
 
-        root_node.text = root_node.text.replace(numDlSlotPart, f"<numDlSlot>{NUMDLSLOT}</numDlSlot>")
-        root_node.text = root_node.text.replace(numDlSymbolPart, f"<numDlSymbol>{NUMDLSYMBOL}</numDlSymbol>")
-        root_node.text = root_node.text.replace(numUlSlotPart, f"<numUlSlot>{NUMULSLOT}</numUlSlot>")
-        root_node.text = root_node.text.replace(numUlSymbolPart, f"<numUlSymbol>{NUMULSYMBOL}</numUlSymbol>")
+            tempResultList = re.findall(
+                f"<{param_name}>\d+</{param_name}>", root_node.text
+            )
+
+            for tempResult in tempResultList:
+                root_node.text = root_node.text.replace(
+                    tempResult, f"<{param_name}>{value}</{param_name}>"
+                )
 
     temp_list = [unique_id, level, root_node.tag, root_node.attrib, root_node.text]
     result_list.append(temp_list)
     unique_id += 1
 
     # 遍歷每個子節點
-    children_node = root_node.getchildren()
-    if len(children_node) == 0:
+    children_node = root_node
+    if len(list(children_node)) == 0:
         return
-    for child in children_node:
+    for child in list(children_node):
         walkData(child, level + 1, result_list)
     return
 
@@ -61,16 +167,30 @@ def replace_text(file_name):
         lines = f.readlines()
 
     for i in range(len(lines)):
-        lines[i] = lines[i].replace("&lt;", "<").replace("&gt;", ">").replace("ns0:", "").replace(":ns0", "")
+        lines[i] = (
+            lines[i]
+            .replace("&lt;", "<")
+            .replace("&gt;", ">")
+            .replace("ns0:", "")
+            .replace(":ns0", "")
+        )
 
     for i in range(len(lines)):
         if "</gnbvs></vsData>" in lines[i]:
             new_lines.append(lines[i].replace("</gnbvs></vsData>", "</gnbvs>"))
             new_lines.append(lines[i].replace("</gnbvs></vsData>", "]]></vsData>"))
-        elif "</vsData>" in lines[i] and lines[i].count("<") == 1 and lines[i].count(">") == 1:
+        elif (
+            "</vsData>" in lines[i]
+            and lines[i].count("<") == 1
+            and lines[i].count(">") == 1
+        ):
             lines[i] = lines[i].replace("</vsData>", "]]></vsData>")
             new_lines.append(lines[i])
-        elif "<vsData>" in lines[i] and lines[i].count("<") == 1 and lines[i].count(">") == 1:
+        elif (
+            "<vsData>" in lines[i]
+            and lines[i].count("<") == 1
+            and lines[i].count(">") == 1
+        ):
             lines[i] = lines[i].replace("<vsData>", "<vsData><![CDATA[")
             new_lines.append(lines[i])
         elif '<vsData><gnbvs xmlns="urn:rdns:com:radisys:nr:gnb">' in lines[i]:
@@ -86,5 +206,6 @@ def replace_text(file_name):
 
 
 if __name__ == "__main__":
-    getXmlData(file_name=input_file_name)
-    replace_text(file_name=output_file_name)
+    if NUMSLOTPARAMS:
+        getXmlData(file_name=input_file_name)
+        replace_text(file_name=output_file_name)
--- test_read_env_params.py
+++
@@ -1,7 +0,0 @@
-import os
-
-# for k, v in os.environ.items():
-#     print(k,':', v)
-#     print('------------------------------------------------')
-
-print(list(os.environ.get('LC_MEASUREMENT')))
